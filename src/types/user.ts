7dcf0745e4f54b7c9e2a1fa5e2cf966d352856ee
Duong <pengu0163@gmail.com>
2026-05-31 20:07:40 +0700

    update

--- src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export type User = {
-    user_id: string;
-    username: string;
-    email: string;
-    full_name: string;
-    membership_type: string;
-    created_at?: string;
-    updated_at?: string;
-}
